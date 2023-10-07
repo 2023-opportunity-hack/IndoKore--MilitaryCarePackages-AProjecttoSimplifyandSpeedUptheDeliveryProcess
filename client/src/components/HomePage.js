69d7dfde1350de71428eb506a763cde31957528e
Fawwaz Firdaus <fawwazfirdaus7@gmail.com>
2023-10-07 16:54:21 -0700

    Current progress

---
+++ client/src/components/HomePage.js
@@ -0,0 +1,19 @@
+// src/components/HomePage.js
+
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="home-page">
+      <h1>Blue Star Mothers</h1>
+      <div className="buttons">
+        <button>Button 1</button>
+        <button>Button 2</button>
+        <button>Button 3</button>
+        <button>Button 4</button>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
