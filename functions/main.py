37bdee1f564647b2d9c781ecd4531ee31614e0b9
Fawwaz Firdaus <fawwazfirdaus7@gmail.com>
2023-10-07 16:09:17 -0700

    Current progress

--- functions/main.py
+++ functions/main.py
@@ -11,3 +11,24 @@ from firebase_admin import initialize_app
 # @https_fn.on_request()
 # def on_request_example(req: https_fn.Request) -> https_fn.Response:
 #     return https_fn.Response("Hello world!")
+
+import firebase_admin
+from firebase_admin import firestore
+from google.cloud import functions
+
+# Initialize Firebase
+firebase_admin.initialize_app()
+
+db = firestore.client()
+
+def hello_world(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
+    """
+    return 'Hello, World!'
